23dd6da149d8a8a12495b7383aefdc31e23bf044
Dina Hafez <dinahafez.8@gmail.com>
2018-06-25 14:21:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,9 @@
 
-# def square_array(array)
-#   # your code here
-#   updated_array = []
-#   array.each do |x|
-#     updated_array.push(x ** 2)
-#   end
-#   return updated_array
-# end
-
 def square_array(array)
   # your code here
   updated_array = []
-  array.each {|x| updated_array << x ** 2}
-  updated_array
+  array.each do |x|
+    updated_array.push(x ** 2)
+  end
+  return updated_array
 end
